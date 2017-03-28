c62031dfaad90365315e25a5d5424eee6f4a33ba
cl389 <cl389@duke.edu>
2017-03-28 11:19:41 -0400

    updateEventList and init

---
+++ backend/init.php
@@ -0,0 +1,12 @@
+<?php
+ $db_name = "ExpenseTracker";
+ $mysql_user = "root";
+ $mysql_pass = "zhangjiayu930501";
+ $server_name = "localhost";
+ $con = mysqli_connect($server_name,$mysql_user,$mysql_pass,$db_name);
+
+ if ($con){
+// echo "connection success\n";
+}
+?>
+      
