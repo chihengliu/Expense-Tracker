300a0ece1f457a09793aff5f6a6ce549be4a531f
cl389 <cl389@duke.edu>
2017-04-12 03:19:09 -0400

    Label entries and legends, chart titles

--- app/src/main/java/com/example/expensetracker/PieActivity.java
+++ app/src/main/java/com/example/expensetracker/PieActivity.java
@@ -1,14 +1,19 @@
 package com.example.expensetracker;
 
+import android.graphics.Paint;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.graphics.Color;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -16,6 +21,7 @@ import java.util.Map;
 import java.util.Set;
 import java.lang.Double;
 import android.content.Intent;
+import android.view.View;
 
 public class PieActivity extends AppCompatActivity {
 
@@ -28,7 +34,7 @@ public class PieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        HashMap<String,Double> map = (HashMap<String, Double>) intent.getSerializableExtra("map");
+        HashMap<String, Double> map = (HashMap<String, Double>) intent.getSerializableExtra("map");
 
         yData = new float[map.size()];
         xData = new String[map.size()];
@@ -36,7 +42,7 @@ public class PieActivity extends AppCompatActivity {
         Iterator iterator = set.iterator();
         int i = 0;
         while (iterator.hasNext()) {
-            Map.Entry mentry = (Map.Entry)iterator.next();
+            Map.Entry mentry = (Map.Entry) iterator.next();
             xData[i] = mentry.getKey().toString();
 
             yData[i] = Float.parseFloat(mentry.getValue().toString());
@@ -45,47 +51,60 @@ public class PieActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_pie);
         Log.d(TAG, "onCreate: starting to create chart");
+
+        //get the chart and set properties
         pieChart = (PieChart) findViewById(R.id.idPieChart);
-//        pieChart.setDescription("Spending by Members");
         pieChart.setDrawHoleEnabled(true);
         pieChart.setRotationEnabled(true);
-        pieChart.setHoleRadius(25f);
-        pieChart.setTransparentCircleAlpha(0);
-        pieChart.setCenterText("Thug Life");
-        pieChart.setCenterTextSize(10);
+        pieChart.setHoleRadius(7);
+        pieChart.setTransparentCircleRadius(10);
         pieChart.setDrawEntryLabels(true);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        pieChart.setEntryLabelTextSize(12f);
 
-    addDataSet(pieChart);
-}
+        //get Description and set it
+        Description d = pieChart.getDescription();
+        d.setText("Spending by members");
+        d.setTextSize(20);
+        d.setXOffset(10);
+        d.setYOffset(20);
+        addDataSet(pieChart);
+    }
+
+
 
     private void addDataSet(PieChart pieChart){
         Log.d(TAG, "addDataSet: started");
-        ArrayList<PieEntry> yEntrys = new ArrayList<>();
-        ArrayList<String> xEntrys = new ArrayList<>();
+        ArrayList<PieEntry> Entrys = new ArrayList<>();
 
         for (int i=0; i<yData.length;i++) {
-            yEntrys.add(new PieEntry(yData[i], i));
+            Entrys.add(new PieEntry(yData[i], xData[i]));
         }
-        for (int i=0; i<xData.length;i++) {
-            xEntrys.add(xData[i]);
-        }
+
         //create data set
-        PieDataSet pieDataSet = new PieDataSet(yEntrys,"Spending");
-        pieDataSet.setSliceSpace(1);
-        pieDataSet.setValueTextSize(12);
+        PieDataSet pieDataSet = new PieDataSet(Entrys,"Spending");
+        pieDataSet.setSliceSpace(3);
 
         //add colors
         ArrayList<Integer> colors = new ArrayList<>();
-        colors.add(Color.GREEN);
-        colors.add(Color.RED);
-        colors.add(Color.CYAN);
+        for (int c: ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
         pieDataSet.setColors(colors);
 
-        //add legend to chart
-        Legend legend = this.pieChart.getLegend();
+        //customize legends
+        Legend l = pieChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setYOffset(10);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setXEntrySpace(7);
+        l.setYEntrySpace(7);
+        l.setFormSize(20);
+
 
         //create pie data object
         PieData pieData = new PieData(pieDataSet);
+        pieData.setValueTextSize(20f);
         this.pieChart.setData(pieData);
         this.pieChart.invalidate();
     }
